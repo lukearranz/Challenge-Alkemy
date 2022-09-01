3d6e5e470333f57c39fdaca07fd8f244c0c8a2dc
lukearranz <lucas.arranz@hotmail.com>
2022-09-01 10:31:33 +0200

    first commit of the challenge

---
+++ src/test/java/com/challenge/alkemy/ChallengeAlkemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.alkemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAlkemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
