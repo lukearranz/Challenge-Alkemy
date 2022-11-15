23d5a9c418ecbc1c168cd0330528e0e3b77fd7e8
lukearranz <83666817+lukearranz@users.noreply.github.com>
2022-11-15 10:34:47 +0100

    Swagger documentation (#7)
    * Api documentation working fine
    
    * changes in the readme file

--- readme.md
+++ readme.md
@@ -16,7 +16,8 @@ mvn clean install && docker-compose up
 www.localhost:8080/swagger-ui.html
 
 // Postman Collection
-https://app.getpostman.com/join-team?invite_code=50d7f4e793c4c48cc69d5391e1861d70&target_code=48c73a34b71a3b39fbf62040a06b5eb6
+https://primerosystem.postman.co/workspace/51686291-a897-447c-82ed-b39ccdcc995d/collection/18340839-60d61dd6-70bd-4264-8df1-141a9cabad09?action=share&creator=18340839
+
 
 // Postman Environment
 https://go.postman.co/workspace/My-Workspace~84f2a75a-be64-4f0a-8b38-0a11cf11fc06/environment/2792137-951b1e05-b588-4df1-82b5-e40b6f8dc777
@@ -27,4 +28,7 @@ User:   ADMINISTRADOR - ADMINISTRADOR
 ```
 
 ## Author
+
 https://www.lucasarranz.com.ar/
+https://www.linkedin.com/lucas-arranz-garcia
+
--- src/main/java/com/challenge/alkemy/ChallengeAlkemyApplication.java
+++ src/main/java/com/challenge/alkemy/ChallengeAlkemyApplication.java
@@ -1,16 +1,11 @@
 package com.challenge.alkemy;
 
-import com.challenge.alkemy.service.EmailSenderService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import net.bytebuddy.build.Plugin;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,9 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @OpenAPIDefinition(info = @Info(title = "Alkemy ApiRest Challenge", version = "1.5", description = "API desarrollada en SpringBoot" +
 		" utilizando JWT para la seguridad, MySql para la persistencia de datos y OpenApi+Swagger para la documentación."))
 public class ChallengeAlkemyApplication {
-
-	@Autowired
-	private EmailSenderService mailService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeAlkemyApplication.class, args);
