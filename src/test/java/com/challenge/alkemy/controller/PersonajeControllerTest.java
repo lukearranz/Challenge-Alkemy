3cb086531b472483b6074b853880cdb4bea9b87d
lukearranz <lucas.arranz@hotmail.com>
2022-11-22 14:17:32 +0100

    deleted some imports

--- src/test/java/com/challenge/alkemy/controller/PersonajeControllerTest.java
+++ src/test/java/com/challenge/alkemy/controller/PersonajeControllerTest.java
@@ -1,23 +1,5 @@
 package com.challenge.alkemy.controller;
 
-import com.challenge.alkemy.entity.Personaje;
-import com.challenge.alkemy.service.PersonajeService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
 class PersonajeControllerTest {
-
-
 
 }
--- src/test/java/com/challenge/alkemy/service/PersonajeServiceImpTest.java
+++ src/test/java/com/challenge/alkemy/service/PersonajeServiceImpTest.java
@@ -1,12 +1,5 @@
 package com.challenge.alkemy.service;
 
-import org.junit.jupiter.api.Test;
-
 class PersonajeServiceImpTest {
-
-
-    void fetchPersonajes() {
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-    }
 
 }
