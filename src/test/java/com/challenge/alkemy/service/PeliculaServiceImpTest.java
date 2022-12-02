2eefef98600e774452adf13ec042fe2cdd5c488e
lukearranz <lucas.arranz@hotmail.com>
2022-12-02 12:09:59 +0100

    working in the peliculaServiceTests

--- src/test/java/com/challenge/alkemy/service/PeliculaServiceImpTest.java
+++ src/test/java/com/challenge/alkemy/service/PeliculaServiceImpTest.java
@@ -4,7 +4,12 @@ import com.challenge.alkemy.entity.Genero;
 import com.challenge.alkemy.entity.Pelicula;
 import com.challenge.alkemy.entity.Personaje;
 import com.challenge.alkemy.entity.dto.peliculaDto.PeliculaMapper;
+import com.challenge.alkemy.entity.dto.peliculaDto.response.PeliculaBuscadaPorParametroResponseDto;
 import com.challenge.alkemy.entity.dto.peliculaDto.response.PeliculaConDetalleResponseDto;
+import com.challenge.alkemy.error.genero.GeneroNotFoundException;
+import com.challenge.alkemy.error.pelicula.PeliculaBuscadaPorParametroIncorrectoException;
+import com.challenge.alkemy.error.pelicula.PeliculaNotFoundException;
+import com.challenge.alkemy.repository.GeneroRepository;
 import com.challenge.alkemy.repository.PeliculaRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,7 +22,9 @@ import java.time.Instant;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -28,6 +35,8 @@ class PeliculaServiceImpTest {
 
     @Mock
     private PeliculaRepository peliculaRepository;
+    @Mock
+    private GeneroRepository generoRepository;
 
     @Spy
     private PeliculaMapper peliculaMapper;
@@ -40,7 +49,7 @@ class PeliculaServiceImpTest {
     void getAllPeliculas() {
 
         // Given
-        when(peliculaRepository.findAll()).thenReturn(Collections.singletonList(buildPelicula()));
+        when(peliculaRepository.findAll()).thenReturn(buildPelicula());
 
         // When
         List<PeliculaConDetalleResponseDto> response = peliculaServiceImp.getAllPeliculas();
@@ -48,7 +57,7 @@ class PeliculaServiceImpTest {
         // Then
         assertThat(response).isNotNull();
         assertThat(response.isEmpty()).isFalse();
-        assertThat(response.get(0).getId()).isEqualTo(buildPelicula().getPeliculaId());
+        assertThat(response.get(0).getId()).isEqualTo(buildPelicula().get(0).getPeliculaId());
         verify(peliculaRepository, times(1)).findAll();
     }
 
@@ -68,13 +77,103 @@ class PeliculaServiceImpTest {
     }
 
     @Test
-    void getPeliculasByOrder() {
+    void canGetPeliculasByOrderAsc() throws PeliculaBuscadaPorParametroIncorrectoException, PeliculaNotFoundException {
 
+        // Given
+        when(peliculaRepository.findAllByOrderByTituloAsc())
+                .thenReturn(Collections.singletonList(buildPelicula().get(0)));
 
+        // When
+        List<PeliculaBuscadaPorParametroResponseDto> response = peliculaServiceImp.getPeliculasByOrder("ASC");
+
+        // Then
+        verify(peliculaRepository, times(1)).findAllByOrderByTituloAsc();
+        assertThat(response).isNotNull();
+        assertThat(response.isEmpty()).isFalse();
+        assertThat(response.get(0).getTitulo()).isEqualTo(buildPelicula().get(0).getTitulo());
     }
 
     @Test
-    void getPeliculasByGeneroId() {
+    void peliculaByOrderAscNotFoundShouldThrowException() {
+
+        // Given
+        when(peliculaRepository.findAllByOrderByTituloAsc()).thenReturn(Collections.emptyList());
+
+        // Then
+        assertThatExceptionOfType(PeliculaNotFoundException.class)
+                .isThrownBy(() -> peliculaServiceImp.getPeliculasByOrder("ASC"));
+        verify(peliculaRepository, times(1)).findAllByOrderByTituloAsc();
+    }
+
+    @Test
+    void canGetPeliculasByOrderDesc() throws PeliculaBuscadaPorParametroIncorrectoException, PeliculaNotFoundException {
+
+        // Given
+        when(peliculaRepository.findAllByOrderByTituloDesc())
+                .thenReturn(Collections.singletonList(buildPelicula().get(1)));
+
+        // When
+        List<PeliculaBuscadaPorParametroResponseDto> response = peliculaServiceImp.getPeliculasByOrder("DESC");
+
+        // Then
+        verify(peliculaRepository, times(1)).findAllByOrderByTituloDesc();
+        assertThat(response).isNotNull();
+        assertThat(response.isEmpty()).isFalse();
+        assertThat(response.get(0).getTitulo()).isEqualTo(buildPelicula().get(1).getTitulo());
+    }
+
+    @Test
+    void peliculaByOrderDescNotFoundShouldThrowException() {
+
+        // Given
+        when(peliculaRepository.findAllByOrderByTituloDesc()).thenReturn(Collections.emptyList());
+
+        // Then
+        assertThatExceptionOfType(PeliculaNotFoundException.class)
+                .isThrownBy(() -> peliculaServiceImp.getPeliculasByOrder("DESC"));
+        verify(peliculaRepository, times(1)).findAllByOrderByTituloDesc();
+    }
+
+    @Test
+    void peliculaByOrderWithWrongParameterShouldThrowException() {
+
+        assertThatExceptionOfType(PeliculaBuscadaPorParametroIncorrectoException.class)
+                .isThrownBy(() -> peliculaServiceImp.getPeliculasByOrder("ASCS"));
+        verify(peliculaRepository, times(0)).findAllByOrderByTituloDesc();
+        verify(peliculaRepository, times(0)).findAllByOrderByTituloAsc();
+    }
+
+    @Test
+    void canGetPeliculasByGeneroId() throws GeneroNotFoundException {
+
+        List<Pelicula> peliculas = buildPelicula();
+        Genero genero = buildGenero();
+        genero.setPeliculas(peliculas);
+
+        // Given
+        when(generoRepository.findById(anyLong())).thenReturn(Optional.of(genero));
+
+        // When
+        List<PeliculaBuscadaPorParametroResponseDto> response = peliculaServiceImp.getPeliculasByGeneroId(1L);
+
+        // Then
+        assertThat(response.get(0).getTitulo()).isEqualTo(genero.getPeliculas().get(0).getTitulo());
+        assertThat(response.get(0).getTitulo()).isEqualTo(genero.getPeliculas().get(0).getTitulo());
+        assertThat(response).isNotNull();
+        assertThat(response.isEmpty()).isFalse();
+        verify(generoRepository, times(1)).findById(anyLong());
+    }
+
+    @Test
+    void getPeliculasByGeneroIdNotFoundThrowException() {
+
+        // Given
+        when(generoRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        // Then
+        assertThatExceptionOfType(GeneroNotFoundException.class)
+                .isThrownBy(() -> peliculaServiceImp.getPeliculasByGeneroId(anyLong()));
+        verify(generoRepository, times(1)).findById(anyLong());
     }
 
     @Test
@@ -109,7 +208,7 @@ class PeliculaServiceImpTest {
     void getPeliculaByTitulo() {
     }
 
-    private Pelicula buildPelicula() {
+    private List<Pelicula> buildPelicula() {
         Pelicula pelicula1 = Pelicula.builder()
                 .peliculaId(1L)
                 .titulo("Ace Ventura")
@@ -129,8 +228,8 @@ class PeliculaServiceImpTest {
                 .personajes(List.of(buildPersonaje()))
                 .genero(buildGenero())
                 .build();
-        return null;
-        // ToDo
+
+        return List.of(pelicula1, pelicula2);
     }
 
     private Genero buildGenero() {
